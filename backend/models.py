768c53cbbe5ac94c1a0cb7519804067b3f05ffed
rafaeln2 <rafaeln_nascimento@hotmail.com>
2026-01-20 14:30:39 -0300

    model sqlalchemy

--- backend/models.py
+++ backend/models.py
@@ -1,22 +1,38 @@
 
 from typing import Optional
-from pydantic import BaseModel, Field, field_validator
-class Product(BaseModel):
-    id: int = Field(...)
-    name: str = Field(..., max_length=50)
-    price: float | None
-    quantity: int | None # required but can be None.
-    origin_place: str | None = None # optional
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+    price = Column(Float)
+    quantity = Column(Integer)
+    origin_place = Column(String(50))    
     
-    @field_validator("price")
-    @classmethod
-    # Equivalent to using Field(..., gt=0)
-    def validate_price(cls, v: Optional[float]) -> Optional[float]:
-        if v is None:
-            return v
-        if v < 0:
-            raise ValueError("Price must be greater than 0")
-        return v
+
+
+# from pydantic import BaseModel, Field, field_validator
+# class Product(BaseModel):
+#     id: int = Field(...)
+#     name: str = Field(..., max_length=50)
+#     price: float | None
+#     quantity: int | None # required but can be None.
+#     origin_place: str | None = None # optional
+    
+#     @field_validator("price")
+#     @classmethod
+#     # Equivalent to using Field(..., gt=0)
+#     def validate_price(cls, v: Optional[float]) -> Optional[float]:
+#         if v is None:
+#             return v
+#         if v < 0:
+#             raise ValueError("Price must be greater than 0")
+#         return v
 
     
     
